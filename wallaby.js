87336d1f53a0ac8a058f335380828d38e061d84b
Nathan Vale <hireme@nathanvale.com>
2019-01-29 00:50:07 +1100

    chore: clean up wallaby.js file

--- wallaby.js
+++ wallaby.js
@@ -1,23 +1,23 @@
 process.env.NODE_ENV = 'test'
 
-const {jest: jestConfig} = require('./src/config') //?
-const babelrc = require('./src/config/babelrc')() //?
+const {jest: jestConfig} = require('./src/config')
+const babelrc = require('./src/config/babelrc')()
 
 const ignores = [
   ...jestConfig.testMatch.map(file => `!${jestConfig.testMatch}`),
   '!/node_modules/',
-] //?
+]
 const files = [
   './src/config/jest.configs',
   ...jestConfig.collectCoverageFrom,
   ...ignores,
-] //?
+]
 const tests = [
   ...jestConfig.testMatch,
   '!/node_modules/',
-  // TODO: raise an issue on wallaby js to find out why this isnt
+  // TODO: raise an issue on wallaby js to find out why this isnt passing
   '!./src/scripts/__tests__/format.js',
-] //?
+]
 
 module.exports = function(wallaby) {
   const config = {
