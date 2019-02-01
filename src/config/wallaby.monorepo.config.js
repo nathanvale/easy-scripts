21241e150496aa30f7ae8e63c44b726cc70053cc
Nathan Vale <nathan.vale@originenergy.com.au>
2019-02-01 16:23:50 +1100

    fix(wallabyMonorepo): use a closure with the setup function instead

--- src/config/wallaby.monorepo.config.js
+++ src/config/wallaby.monorepo.config.js
@@ -1,48 +1,47 @@
-// Wallaby rocks
 // eslint-disable-next-line no-unused-vars
-module.exports = ({scope = '', name = 'My monorepo'} = {}) => wallaby => ({
-  name,
-  env: {
-    type: 'node',
-    runner: 'node',
-  },
-  testFramework: 'jest',
-  files: [
-    './jest.settings.js',
-    './jest.config.js',
-    './jest.setup.js',
-    'tsconfig.json',
-    'packages/*/src/**',
-    '!packages/**/*.spec.{ts,tsx}',
-    '!**/*.d.ts',
-    '!**/*.{snap}',
-    '!packages/**/coverage/**',
-    '!packages/**/node_modules/**',
-  ],
-  compilers: {
-    '*.js': wallaby.compilers.babel({
-      babel: require('@babel/core'),
-    }),
-    'packages/**/*.{ts,tsx}': wallaby.compilers.babel({
-      babel: require('@babel/core'),
-    }),
-  },
-  tests: ['packages/*/src/**/*.spec.{ts,tsx}'],
-  // eslint-disable-next-line func-names
+module.exports = ({scope = '', name = 'My monorepo'} = {}) => wallaby => {
   // eslint-disable-next-line no-shadow
-  setup: wallaby => {
+  const setup = wallaby => {
     let jestConfig = global._modifiedJestConfig
     if (!jestConfig) {
       // eslint-disable-next-line no-multi-assign
       jestConfig = global._modifiedJestConfig = require('./jest.config.js')
       jestConfig.moduleNameMapper = {
-        [`^${
-          // eslint-disable-next-line babel/no-invalid-this
-          this.scope
-        }/([a-zA-Z0-9_-]+)$`]: `${process.cwd()}/packages/$1/src/index.js`,
+        [`^${scope}/([a-zA-Z0-9_-]+)$`]: `${process.cwd()}/packages/$1/src/index.js`,
       }
       jestConfig.setupTestFrameworkScriptFile = './jest.setup.js'
     }
     wallaby.testFramework.configure(jestConfig)
-  },
-})
+  }
+
+  return {
+    name,
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+    testFramework: 'jest',
+    files: [
+      './jest.settings.js',
+      './jest.config.js',
+      './jest.setup.js',
+      'tsconfig.json',
+      'packages/*/src/**',
+      '!packages/**/*.spec.{ts,tsx}',
+      '!**/*.d.ts',
+      '!**/*.{snap}',
+      '!packages/**/coverage/**',
+      '!packages/**/node_modules/**',
+    ],
+    compilers: {
+      '*.js': wallaby.compilers.babel({
+        babel: require('@babel/core'),
+      }),
+      'packages/**/*.{ts,tsx}': wallaby.compilers.babel({
+        babel: require('@babel/core'),
+      }),
+    },
+    tests: ['packages/*/src/**/*.spec.{ts,tsx}'],
+    setup,
+  }
+}
