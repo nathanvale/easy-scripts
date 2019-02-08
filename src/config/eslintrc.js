fa345779bfba0caef1fc67910483b56d89640eb9
Nathan Vale <nathan.vale@originenergy.com.au>
2019-02-09 05:59:59 +1100

    build(esliit): switch to eslint-config-ndv

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -2,10 +2,10 @@ const {ifAnyDep} = require('../utils')
 
 module.exports = {
   extends: [
-    require.resolve('eslint-config-kentcdodds'),
-    require.resolve('eslint-config-kentcdodds/jest'),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/jsx-a11y')),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/react')),
+    require.resolve('eslint-config-ndv'),
+    require.resolve('eslint-config-ndv/jest'),
+    ifAnyDep('react', require.resolve('eslint-config-ndv/jsx-a11y')),
+    ifAnyDep('react', require.resolve('eslint-config-ndv/react')),
   ].filter(Boolean),
   rules: {},
 }
