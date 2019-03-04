ef59d209dd69f24adc4ac078b441bf4a01ed526d
Nathan Vale <hireme@nathanvale.com>
2019-03-05 08:04:55 +1100

    chore: wip

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -2,11 +2,17 @@ const {packageManager} = require('../jsonate/')
 
 const {ifAnyDep} = packageManager()
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
+    'plugin:@typescript-eslint/recommended',
     require.resolve('eslint-config-ndv'),
     require.resolve('eslint-config-ndv/jest'),
     ifAnyDep('react', require.resolve('eslint-config-ndv/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-ndv/react')),
   ].filter(Boolean),
-  rules: {},
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+  },
 }
