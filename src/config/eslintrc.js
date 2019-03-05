72ad4e6961d6e50f3cc4a8a40a2d6f8864904ffa
Nathan Vale <nathan.vale@originenergy.com.au>
2019-03-05 15:46:12 +1100

    chore: wip

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,18 +1,16 @@
 const {packageManager} = require('../jsonate/')
+const {ifTypescriptProject} = require('../utils')
 
 const {ifAnyDep} = packageManager()
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     require.resolve('eslint-config-ndv'),
     require.resolve('eslint-config-ndv/jest'),
+    ifTypescriptProject(require.resolve('eslint-config-ndv/typescript')),
     ifAnyDep('react', require.resolve('eslint-config-ndv/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-ndv/react')),
   ].filter(Boolean),
   rules: {
-    indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
+    'import/no-unresolved': ifTypescriptProject('off', 'error'),
   },
 }
