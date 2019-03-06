2519ca058851d6e6fe7db4378a31a61e588e243c
Nathan Vale <nathan.vale@originenergy.com.au>
2019-03-06 15:59:56 +1100

    fix: point to eslint-config-nathanvale for linting

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -4,11 +4,11 @@ const {ifTypescriptProject} = require('../utils')
 const {ifAnyDep} = packageManager()
 module.exports = {
   extends: [
-    require.resolve('eslint-config-ndv'),
-    require.resolve('eslint-config-ndv/jest'),
-    ifTypescriptProject(require.resolve('eslint-config-ndv/typescript')),
-    ifAnyDep('react', require.resolve('eslint-config-ndv/jsx-a11y')),
-    ifAnyDep('react', require.resolve('eslint-config-ndv/react')),
+    require.resolve('eslint-config-nathanvale'),
+    require.resolve('eslint-config-nathanvale/jest'),
+    ifTypescriptProject(require.resolve('eslint-config-nathanvale/typescript')),
+    ifAnyDep('react', require.resolve('eslint-config-nathanvale/jsx-a11y')),
+    ifAnyDep('react', require.resolve('eslint-config-nathanvale/react')),
   ].filter(Boolean),
   rules: {
     'import/no-unresolved': ifTypescriptProject('off', 'error'),
